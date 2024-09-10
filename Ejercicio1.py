1817bfc7a6dbdf68ab26722e5ce0230d2c6fa367
dayanamh1998 <dayanamh1998@gmail.com>
2024-09-10 11:10:55 -0500

    Se realiza el ejercicio nuemro uno  del taller ya esta bueno

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,1 +1,10 @@
-  
+def generarSerie(limite):
+    serie = [5, 8]
+    while len(serie) < limite:
+        siguienteValor = serie[-1] + serie[-2]
+        if siguienteValor != 13:
+            serie.append(siguienteValor)
+    return serie
+
+serie = generarSerie(100)  # Corregir el nombre de la función aquí
+print(serie)
